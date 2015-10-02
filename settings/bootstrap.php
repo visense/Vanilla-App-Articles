ee9a8752929efbf0a74c843b9c78221b3ff299b3
austins <austins@users.noreply.github.com>
2015-10-02 11:15:40 -0700

    Initial develop branch created (rewrite with Vanilla models)

--- settings/bootstrap.php
+++ settings/bootstrap.php
@@ -1,22 +1,11 @@
-<?php defined('APPLICATION') or exit();
+<?php
 /**
- * Copyright (C) 2015  Austin S.
+ * Register library classes and interfaces in the auto-loader.
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * @copyright 2015 Austin S.
+ * @license http://www.opensource.org/licenses/gpl-2.0.php GNU GPL v2
  */
 
-// Register library classes and interfaces in the auto-loader.
 $Map = Gdn_Autoloader::MAP_LIBRARY;
 $Context = Gdn_Autoloader::CONTEXT_APPLICATION;
 $Path = PATH_APPLICATIONS . DS . 'articles' . DS . 'library';
@@ -24,5 +13,5 @@ $Options = array('Extension' => 'articles');
 
 Gdn_Autoloader::RegisterMap($Map, $Context, $Path, $Options);
 
-// Include the functions.render.php file.
+// Include the functions.render.php file
 require_once(PATH_APPLICATIONS . DS . 'articles' . DS . 'library' . DS . 'functions.render.php');
--- views/modules/articlesdashboard.php
+++
@@ -1,8 +0,0 @@
-<?php defined('APPLICATION') or exit();
-
-echo '<div class="BoxButtons BoxArticlesDashboard">';
-echo Anchor(T('Articles Dashboard'), '/compose',
-    'Button Action Big Primary BigButton ArticlesDashboard');
-
-Gdn::Controller()->FireEvent('AfterArticlesDashboardButton');
-echo '</div>';
